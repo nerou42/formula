3588207c8a36039e54cc5fcf6289affeebc13118
Timo Lehnertz <timo.lehnertz@nerou.de>
2025-12-01 18:26:14 +0100

    Updated bench test

--- test/benchmark/BenchManyExpressions.php
+++ test/benchmark/BenchManyExpressions.php
@@ -8,6 +8,7 @@ use TimoLehnertz\formula\Formula;
 use TimoLehnertz\formula\procedure\Scope;
 use TimoLehnertz\formula\type\Value;
 use PhpBench\Attributes as Bench;
+use TimoLehnertz\formula\type\FloatType;
 use TimoLehnertz\formula\type\FloatValue;
 
 enum MeasurementInterpolation: string {
@@ -25,39 +26,32 @@ class BenchManyExpressions {
 
     private Formula $formula;
 
-
     public function setUp(): void {
         $scope = new Scope();
         $scope->definePHP(true, 'MeasurementInterpolation', MeasurementInterpolation::class);
         $scope->definePHP(true, 'TagBehavior', TagBehavior::class);
         $scope->definePHP(true, 'passthroughValue', $this->passthroughValue(...));
-        $scope->definePHP(true, 'getMeasurement', $this->getMeasurement(...));
+        $scope->definePHP(true, 'getMeasurement', $this->getMeasurement(...), null, new FloatType());
         $this->formula = new Formula('((((getMeasurement("S1", null, null, "P0D", TagBehavior.PASS_ON)+getMeasurement("S2", null, null, "P0D", TagBehavior.PASS_ON))/2)<0)?null:((getMeasurement("S1", null, null, "P0D", TagBehavior.PASS_ON)+getMeasurement("S2", null, null, "P0D", TagBehavior.PASS_ON))/2))', $scope);
     }
 
+    /**
+     * Simulates the formula overhead of calculating a sensor for every minute for 6 months.
+     */
     #[Bench\BeforeMethods('setUp')]
-    #[Bench\Revs(10)]
+    #[Bench\Revs(2)]
     public function benchPassthroughValue() {
-        // ini_set('memory_limit', '8M');
-        for ($i=0; $i < 1000; $i++) { 
+        ini_set('memory_limit', '2000M');
+        for ($i=0; $i < 60*24*30*6; $i++) {
             $this->formula->calculate();
         }
     }
-
-    // #[Bench\Revs(1000)]
-    // public function benchCallUserFunc() {
-        
-    //     // $func = $this->getMeasurement(...);
-    //     // for ($i=0; $i < 100000; $i++) { 
-    //     //     call_user_func_array($func, ["S1", MeasurementInterpolation::FILL, false, new \DateInterval('P1D')]);
-    //     // }
-    // }
 
     public function passthroughValue(Value $input): Value {
         return $input;
     }
 
-    public function getMeasurement(string $sensorID, ?MeasurementInterpolation $interpolation = null, ?bool $extrapolation = null, \DateInterval|int $timeOffsetToPast = new \DateInterval('P0D'), TagBehavior $tagBehavior = TagBehavior::PASS_ON): float {
-        return 1;
+    public function getMeasurement(string $sensorID, ?MeasurementInterpolation $interpolation = null, ?bool $extrapolation = null, \DateInterval|int $timeOffsetToPast = new \DateInterval('P0D'), TagBehavior $tagBehavior = TagBehavior::PASS_ON): FloatValue {
+        return new FloatValue(1);
     }
 }
